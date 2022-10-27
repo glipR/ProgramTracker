3e859eda9c0ed4c0839e6c92028373a63f953278
glipR <37640160+glipR@users.noreply.github.com>
2022-10-27 18:58:26 +1100

    Add userinfo.

--- server/account/models.py
+++ server/account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserInfo(models.Model):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="info")
+
+    def __str__(self):
+        return f"{self.user.username} Info"
