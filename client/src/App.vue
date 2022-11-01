2f42edcc801d4fdf674a9588d3dbbf058044beb2
glipR <37640160+glipR@users.noreply.github.com>
2022-11-01 11:45:05 +1100

    Good toolbar.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,64 +1,85 @@
 <template>
   <v-app>
 
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      height="68px;"
+    >
+      <div class="d-flex align-center">
+        <v-icon
+          large
+          color="white"
+          aria-hidden="false"
+        >mdi-balloon</v-icon>
+
+        <h2 class="ml-2">ProgramTracker</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-card rounded="0">
+        <v-card-title>
+          <v-avatar
+            size="48"
+            rounded="lg"
+            class="text-center"
+            style="margin-right: 10px;"
+          >
+            <img
+              alt="user"
+              src="./assets/logo.png"
+              v-if="user_image"
+            >
+            <div v-else class="white--text" style="background-color: blue; width: 100%; height: 100%; font-size: 24px;">
+              <p style="margin: 0; padding-top: 8px; padding-bottom: 8px">JG</p>
+            </div>
+          </v-avatar>
+          <div style="display: block">
+            <div>
+              {{ user_data.username }}
+            </div>
+            <v-row class="text-center">
+              <v-col cols="4">
+                <v-icon
+                  color="red darken-2"
+                  aria-hidden="false"
+                  aria-label="Streak"
+                >mdi-fire</v-icon>
+                <span style="font-size: 14px; width: 33px; display: inline-block">
+                  x
+                </span>
+              </v-col>
+              <v-col cols="4">
+                <v-icon
+                  color="yellow darken-2"
+                  aria-hidden="false"
+                  aria-label="Coins"
+                >mdi-checkbox-blank-circle</v-icon>
+                <span style="font-size: 14px; width: 33px; display: inline-block">
+                  {{ user_data.info.coins }}
+                </span>
+              </v-col>
+              <v-col cols="4">
+                <v-icon
+                  color="blue darken-2"
+                  aria-hidden="false"
+                  aria-label="Freezes"
+                >mdi-hexagon-slice-6</v-icon>
+                <span style="font-size: 14px; width: 33px; display: inline-block">
+                  {{ user_data.info.freezes }}
+                </span>
+              </v-col>
+            </v-row>
+          </div>
+        </v-card-title>
+      </v-card>
+    </v-app-bar>
+
     <v-main>
       <HelloWorld/>
     </v-main>
-
-    <!-- Ensure toolbar is rendered on-top. -->
-    <v-card
-      elevation="1"
-      style="position: fixed; right: 0; top: 0;"
-    >
-      <v-card-title>
-        <v-avatar
-          size="48"
-          rounded="lg"
-          class="text-center"
-          style="margin-right: 10px;"
-        >
-          <img
-            alt="user"
-            src="./assets/logo.png"
-            v-if="user_image"
-          >
-          <div v-else class="white--text" style="background-color: blue; width: 100%; height: 100%; font-size: 24px;">
-            <p style="margin: 0; padding-top: 8px; padding-bottom: 8px">JG</p>
-          </div>
-        </v-avatar>
-        <v-sheet>
-          <div>
-            {{ user_data.username }}
-          </div>
-          <v-row class="text-center">
-            <v-col cols="4">
-              <v-icon
-                color="red darken-2"
-              >mdi-fire</v-icon>
-              <span style="font-size: 14px; width: 33px; display: inline-block">
-                x
-              </span>
-            </v-col>
-            <v-col cols="4">
-              <v-icon
-                color="yellow darken-2"
-              >mdi-checkbox-blank-circle</v-icon>
-              <span style="font-size: 14px; width: 33px; display: inline-block">
-                {{ user_data.info.coins }}
-              </span>
-            </v-col>
-            <v-col cols="4">
-              <v-icon
-                color="blue darken-2"
-              >mdi-hexagon-slice-6</v-icon>
-              <span style="font-size: 14px; width: 33px; display: inline-block">
-                {{ user_data.info.freezes }}
-              </span>
-            </v-col>
-          </v-row>
-        </v-sheet>
-      </v-card-title>
-    </v-card>
 
   </v-app>
 </template>
@@ -96,3 +117,13 @@ export default Vue.extend({
   }),
 });
 </script>
+
+<style>
+.v-toolbar__content {
+  padding-right: 0;
+}
+.v-card__title {
+  padding-bottom: 0;
+  padding-top: 0;
+}
+</style>
--- client/src/ProblemApp.vue
+++ client/src/ProblemApp.vue
@@ -4,37 +4,70 @@
       app
       color="primary"
       dark
+      height="68px;"
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+        <v-icon
+          large
+          color="white"
+          aria-hidden="false"
+        >mdi-balloon</v-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h2 class="ml-2">ProgramTracker</h2>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-card rounded="0">
+        <v-card-title>
+          <v-avatar
+            size="48"
+            rounded="lg"
+            class="text-center"
+            style="margin-right: 10px;"
+          >
+            <img
+              alt="user"
+              src="./assets/logo.png"
+              v-if="user_image"
+            >
+            <div v-else class="white--text" style="background-color: blue; width: 100%; height: 100%; font-size: 24px;">
+              <p style="margin: 0; padding-top: 8px; padding-bottom: 8px">JG</p>
+            </div>
+          </v-avatar>
+          <div style="display: block">
+            <div>
+              {{ user_data.username }}
+            </div>
+            <v-row class="text-center">
+              <v-col cols="4">
+                <v-icon
+                  color="red darken-2"
+                >mdi-fire</v-icon>
+                <span style="font-size: 14px; width: 33px; display: inline-block">
+                  x
+                </span>
+              </v-col>
+              <v-col cols="4">
+                <v-icon
+                  color="yellow darken-2"
+                >mdi-checkbox-blank-circle</v-icon>
+                <span style="font-size: 14px; width: 33px; display: inline-block">
+                  {{ user_data.info.coins }}
+                </span>
+              </v-col>
+              <v-col cols="4">
+                <v-icon
+                  color="blue darken-2"
+                >mdi-hexagon-slice-6</v-icon>
+                <span style="font-size: 14px; width: 33px; display: inline-block">
+                  {{ user_data.info.freezes }}
+                </span>
+              </v-col>
+            </v-row>
+          </div>
+        </v-card-title>
+      </v-card>
     </v-app-bar>
 
     <v-main>
@@ -46,6 +79,9 @@
 <script lang="ts">
 import Vue from 'vue';
 import ProblemView from './components/ProblemView.vue';
+import axios from 'axios';
+
+const API_URL = "http://127.0.0.1:8000/api/";
 
 export default Vue.extend({
   name: 'App',
@@ -54,8 +90,32 @@ export default Vue.extend({
     ProblemView,
   },
 
+  mounted() {
+    this.user_id = document.getElementById("user_id_read")?.innerHTML || "";
+    axios
+      .get(API_URL + "users/" + this.user_id + "/")
+      .then(response => {
+        this.user_data = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  },
+
   data: () => ({
-    //
+    user_image: "",
+    user_id: "",
+    user_data: {},
   }),
 });
 </script>
+
+<style>
+.v-toolbar__content {
+  padding-right: 0;
+}
+.v-card__title {
+  padding-bottom: 0;
+  padding-top: 0;
+}
+</style>
